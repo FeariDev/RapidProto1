bfd6d905e182ff1b37756b20c78865d9d6f9d0e0
Pewewe2212 <petteri.viitalahde@gmail.com>
2025-08-21 12:03:47 +0300

    Added a game manager and the bars for winning and losing
    + UI that has the bars for winning/losing
    + GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void Event(int eventNumber)
+    {
+        if (eventNumber == 0)
+        { 
+            //win
+        }
+        if (eventNumber == 1)
+        {
+            //loss
+        }
+    }
+}
