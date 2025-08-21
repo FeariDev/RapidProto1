1f6dc54ff4a903cf47c80886a37be9450f274235
Pewewe2212 <petteri.viitalahde@gmail.com>
2025-08-21 12:31:26 +0300

    Added code for winning/losing, added TMPro
    + Menu for use after a stage
    + Added winning, losing and retrying to game manager
    + Added TMPro

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject retry;
+    [SerializeField] private GameObject next;
+    [SerializeField] private TextMeshProUGUI menuText;
+
     public void Event(int eventNumber)
     {
+        menu.SetActive(true);
+        retry.SetActive(true);
         if (eventNumber == 0)
-        { 
+        {
             //win
+            menuText.text = "Stage Cleared";
+            next.SetActive(true);
         }
         if (eventNumber == 1)
         {
             //loss
+            menuText.text = "Stage Failed";
         }
+    }
+
+    public void NextStage(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
