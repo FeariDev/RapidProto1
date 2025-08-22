c150cd93053e64be82b311af526c0f432c0792c7
Pewewe2212 <petteri.viitalahde@gmail.com>
2025-08-22 10:42:39 +0300

    [TEMP] Hole testing
    +Hole script

--- Assets/Scripts/Bars.cs
+++ Assets/Scripts/Bars.cs
@@ -9,20 +9,11 @@ public class Bars : MonoBehaviour
     [SerializeField] private Image progressBar;
     public GameManager manager;
 
-    private void Update()
-    {
-        //For testing only
-        if (Input.GetMouseButtonDown(0))
-        {
-            AddScore(1);
-        }
-    }
-
     public void AddScore(int amount)
     {
         currentAmount += amount;
         progressBar.rectTransform.localScale += new Vector3(0f, amount / 10f, 0f); //reaches 80
-        progressBar.rectTransform.localPosition += new Vector3(0f, amount*5f, 0f); //for every /10 in scale, add *5 position
+        progressBar.rectTransform.localPosition += new Vector3(0f, amount*5f, 0f); //Normal scane /10 scale, *5 position
         if (currentAmount >= amountNeededForEvent)
         {
             manager.Event(eventNumber);
