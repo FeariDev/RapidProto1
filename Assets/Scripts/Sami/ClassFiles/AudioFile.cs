4d4288cd403bca134b706f17698cfcb00e01850f
Sami Nissinen <s2300890@edu.bc.fi>
2025-08-22 12:56:17 +0300

    Audio Emiter Componentti Luotu

---
+++ Assets/Scripts/Sami/ClassFiles/AudioFile.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class AudioEmiterFile
+{
+    public string name;
+    public AudioClip clip;
+    public float volume = 1.0f;
+    public float pitch;
+}
